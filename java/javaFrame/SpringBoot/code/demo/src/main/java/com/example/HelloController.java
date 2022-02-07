957ee7185abc5b270a3be5a43e3b71b6bc675e29
Yu Tao <2843980878@qq.com>
2022-02-07 13:21:33 +0800

    add ./java/javaFrame

---
+++ java/javaFrame/SpringBoot/code/demo/src/main/java/com/example/HelloController.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+    @RequestMapping("/hello")
+    public String handle01(){
+        return "Hello, Spring Boot 2!";
+    }
+
+
+}
---
+++ java/javaFrame/SpringBoot/code/demo/src/main/java/com/example/MainApplication.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 主程序类
+ * @SpringBootApplication：这是一个SpringBoot应用
+ */
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class,args);
+    }
+}
